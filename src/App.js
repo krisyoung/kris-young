71db7b59523d82a4a60dbc81ab02d40dd56b531c
Kris Young <kris@fanduel.com>
2018-09-18 16:07:58 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Nothing to see here... move along!
+      </div>
+    );
+  }
+}
+
+export default App;
